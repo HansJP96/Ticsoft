a8dd5bb21ffd15c0062db469c2b697b2fe2fa991
Hansee Jimenez <hansee1996@gmail.com>
2021-10-23 22:39:55 -0500

    Cambio puertot

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 
 require("./app/routes/tecsoft.routes")(app);
 
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3100;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
